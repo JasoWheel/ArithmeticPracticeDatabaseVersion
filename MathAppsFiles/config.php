dd1c0e0e712f126515085900eb01fa34f61be751
JasoWheel <42422348+JasoWheel@users.noreply.github.com>
2023-12-25 12:47:52 -0600

    Update config.php

--- MathAppsFiles/config.php
+++ MathAppsFiles/config.php
@@ -1,8 +1,8 @@
 <?php if(session_status() == PHP_SESSION_NONE) if(session_status() == PHP_SESSION_NONE) session_start();
-$databaseHost='localhost';
-$databaseUser='root'; 
-$databasePassword='qP29Gh!R'; 
-$databaseName='MathAppsData';
+$databaseHost='';
+$databaseUser=''; 
+$databasePassword=''; 
+$databaseName='';
 try {
   $conn = new PDO("mysql:host=".$databaseHost.";dbname=".$databaseName, $databaseUser, $databasePassword);
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
